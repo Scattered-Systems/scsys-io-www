4de724852464dd1e1ee6596ef0264f1317aeaada
Joe McCain III <jo3mccain@icloud.com>
2024-11-28 10:29:26 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+/*
+  Appellation: middleware <module>
+  Contrib: @FL03
+*/
+
+import type { NextRequest } from 'next/server';
+
+export const middleware = async (request: NextRequest) => {
+  // Add your middleware code here.
+  return request;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
