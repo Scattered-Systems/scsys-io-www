99f4b366bfec920ea5eaabb70313d0b6cb7e9b7c
Joe McCain III <jo3mccain@icloud.com>
2024-12-03 22:40:36 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -4,15 +4,24 @@
 */
 import * as React from 'react';
 import About from '@/features/about/data/about.mdx';
-import ContentSection from '@/components/common/content';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 export const runtime = 'edge';
 
 const Page: React.FC = () => {
   return (
-    <ContentSection flavor="accent">
-      <About />
-    </ContentSection>
+    <>
+      <Card className="flex flex-col flex-1 w-full">
+        <CardHeader>
+          <CardTitle>About</CardTitle>
+          <CardDescription>A little bit about us!</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <About />
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </>
   );
 }
 Page.displayName = 'AboutPage';
