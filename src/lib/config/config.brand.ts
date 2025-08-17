e6419c0786845a1a84a31036e8541f0db0add0f0
FL03 <jo3mccain@icloud.com>
2025-08-17 14:36:20 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ src/lib/config/config.brand.ts
@@ -0,0 +1,15 @@
+type Brand = {
+  name: string;
+  abbreviation: string;
+  logo: string;
+  icon: string;
+  url: string;
+}
+
+export const brand: Brand = {
+  name: 'Scattered-Systems',
+  abbreviation: 'scsys',
+  logo: 'scsys',
+  icon: 'scsys',
+  url: 'https://scsys.io',
+}
--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,7 +1,8 @@
-/*
-  Appellation: logger <utils>
-  Contrib: @FL03
-*/
+/**
+ * Created At: 2025.08.17:14:19:04
+ * @author - @FL03
+ * @file - logger.ts
+ */
 import pino from 'pino';
 
 export const logger = pino();
